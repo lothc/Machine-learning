2aacd60614434b7fda324d01bf4af8e24e0a1345
lothc <linh@Linhs-MacBook-Pro.local>
2014-12-29 10:38:53 -0800

    Added classification tree

--- collapsePrediction.R
+++ collapsePrediction.R
@@ -1,10 +1,10 @@
 # by Christophe Loth
 # Written on June 11, 2013
 #
-# This code fits multivariate supervised learning models (logistic regression, Naive Bayes, 
-# SVM with various kernels, Neural network, ...) to predict structural collapse based on
+# This code fits multivariate supervised learning models for classification (logistic regression, Naive Bayes, 
+# SVM with various kernels, Neural network, classification tree) to predict structural collapse based on
 # measures of ground motion intensity. A 2D example is shown and illustrated at the end. For simplicity, 
-# no train/test split of the data is performed.
+# no train/test split of the data or cross-validation is performed.
 #
 #
 # Inputs:
@@ -14,11 +14,11 @@
 #
 # Output:
 #   - List of fitted models
-#      $logRegModel
-#      $naiveBayes
-#      $svmModelLinear
-#      $svmModelGaussian
-#      $nnet2
+#      $logRegModel: logistic regression model (classification)
+#      $naiveBayes: naive Bayes model
+#      $svmModelLinear: support vector machine with linear kernel
+#      $svmModelGaussian: support vector machine with Gaussian kernel
+#      $nnet3: neural network with a single 3-unit hidden layer
 
 
 rm(list = ls())
@@ -27,7 +27,7 @@ rm(list = ls())
 library(e1071)
 library(nnet)
 library(rpart)
-
+#library(randomForest)
 
 set.seed(100)
 
@@ -41,23 +41,28 @@ fitModels=function(collapseData){
   # Fit various models and output in list object
   modelList=list()
   
-  modelList$logRegModel = glm( y ~ x + 1 , family=binomial(link = "logit"))
+  modelList$logRegModel = glm( y ~ x + 1 , family=binomial(link = "logit"),
+                               model = TRUE, method = "glm.fit")
   modelList$svmModelLinear = svm( x , y , type='C-classification', 
-                                 kernel='linear',scale=FALSE)
+                                 kernel='linear', scale=FALSE, degree = 3, cost = 1, nu = 0.5,
+                                 tolerance = 0.001, epsilon = 0.1, shrinking = TRUE, cross = 0)
   modelList$svmModelGaussian = svm( x , y , type='C-classification', 
-                                   kernel='radial',scale=FALSE)
-  modelList$nnet2 = nnet( x , y , size=3)
-  modelList$nB=naiveBayes( x , as.factor(y)) 
+                                   kernel='radial',scale=FALSE, degree = 3, cost = 1, nu = 0.5,
+                                   tolerance = 0.001, epsilon = 0.1, shrinking = TRUE, cross = 0)
+  modelList$nnet3 = nnet( x , y , size=3, linout = FALSE, entropy = FALSE, softmax = FALSE,
+                          censored = FALSE, skip = FALSE, rang = 0.7, decay = 0,
+                          maxit = 100, MaxNWts = 1000,abstol = 1.0e-4, reltol = 1.0e-8)
+  modelList$nB=naiveBayes( x , as.factor(y), laplace = 0) 
+  modelList$classifTree = rpart (as.formula(paste(names(collapseData)[p+1],"~.",sep="")),
+                                 data=as.data.frame(collapseData),
+                                 control= rpart.control(cp = 0.01, maxcompete = 4, maxdepth = 30)) 
   
-
+  #randomForest ( x, as.factor(y),ntree=100)
 
   return(modelList)
   
   
 }
-
-
-
 
 
 
@@ -86,8 +91,7 @@ attach(modelList)
 # Plot the results
 source('filled.contour3.R')
 # Custom function to plot nonlinear decision boundaries
-plotFun=function(model){
-  zpred=predict(model,as.matrix(xMatPlot))
+plotFun=function(zpred){
   zpred=matrix(as.numeric(as.vector(zpred)),nrow=length(xplot),ncol=length(xplot))
   plot.new()
   filled.contour3(xplot,xplot,zpred,nlevels=2,col=rev(cm.colors(n=2, alpha=0.5)),  
@@ -98,7 +102,7 @@ plotFun=function(model){
   )
   points(x[maskCol,1],x[maskCol,2],pch=4,col="red",cex=0.75)
   points(x[!maskCol,1],x[!maskCol,2],pch=1,col="green",cex=0.75)
-  
+  legend("bottomleft",c("Non-collapse","Collapse"),pch=c(1,4),col=c("green","red"),cex=1.5,bg='white')
 }
 
 pdf('fittedModels.pdf',width=18,height=12)
@@ -110,17 +114,16 @@ plot(x[,1],x[,2],type="n",xlim=c(0,2),ylim=c(0,2),xlab='Ground motion intensity 
 maskCol=(y==1)
 points(x[maskCol,1],x[maskCol,2],pch=4,col="red",cex=0.75)
 points(x[!maskCol,1],x[!maskCol,2],pch=1,col="green",cex=0.75)
+legend("bottomleft",c("Non-collapse","Collapse"),pch=c(1,4),col=c("green","red"),cex=1.5,bg='white')
 title(main='Collapse data',cex.main=2)
-
-
-
 
 
 xplot=seq(0,2,0.01)
 xMatPlot=expand.grid(xplot,xplot)
 names(xMatPlot)=c('V1','V2')
 # SVM with linear kernel + logistic regression 
-plotFun(svmModelLinear)
+zpred=predict(svmModelLinear,as.matrix(xMatPlot))
+plotFun(zpred)
 title(main='Logistic regression + SVM with linear kernel',cex.main=2)
 
 lines(xplot,-1/logRegModel$coeff[[3]]*(logRegModel$coeff[1]+logRegModel$coeff[2]*xplot),
@@ -140,19 +143,29 @@ lines(c(x1min,x1max),  (svmModelLinear$rho-1-coef1*c(x1min, x1max))/coef2, lty=2
 
 
 # Naive Bayes
-plotFun(nB)
+zpred=predict(nB,as.matrix(xMatPlot))
+plotFun(zpred)
 title(main='Naive Bayes',cex.main=2)
 
 
 # SVM with Gaussian kernel
-plotFun(svmModelGaussian)
+zpred=predict(svmModelGaussian,as.matrix(xMatPlot))
+plotFun(zpred)
 title(main='SVM with Gaussian kernel',cex.main=2)
 
-# Neural network of size 2
-plotFun(nnet2)
+# Neural network of size 3
+zpred=predict(nnet3,as.matrix(xMatPlot))
+plotFun(zpred)
 title(main='Neural network with 1 hidden layer of 3 units',cex.main=2)
 
 
+# Classification tree
+zpred=predict(classifTree,as.data.frame(xMatPlot),type="matrix")
+plotFun(zpred)
+title(main='Classification tree',cex.main=2)
 
+# Random forest
+# plotFun(RF)
+# title(main='Random forest',cex.main=2)
 
 dev.off()
